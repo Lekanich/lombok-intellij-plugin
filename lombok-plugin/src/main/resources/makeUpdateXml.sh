04207cb9ec86a41406587c81b697713d886ad6d7
Suburban Squirrel <subsquirrel@gmail.com>
2014-07-23 16:32:04 +0300

    FW-419. makeUpdateXml.sh

--- lombok-plugin/src/main/resources/makeUpdateXml.sh
+++ lombok-plugin/src/main/resources/makeUpdateXml.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?><plugins>" > updatePlugins.xml
-cat *Update.xml >> updatePlugins.xml
-echo "</plugins>" >> updatePlugins.xml
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?><plugins>" > $1/updatePlugins.xml
+cat $1/*Update.xml >> $1/updatePlugins.xml
+echo "</plugins>" >> $1/updatePlugins.xml
